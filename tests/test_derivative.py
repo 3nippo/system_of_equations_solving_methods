569245bdde719bec324d6b6126749165cfe40234
3nippo <mopsyaka13@gmail.com>
2020-05-20 09:56:11 +0300

    fixed

--- tests/test_derivative.py
+++ tests/test_derivative.py
@@ -2,10 +2,17 @@ import context
 import approx
 
 
-X = [0, 0.1, 0.2, 0.3, 0.4]
-Y = [1, 1.1052, 1.2214, 1.3499, 1.4918]
+X = [0, 0.5, 1, 1.5, 2]
+Y = [0, 0.97943, 1.8415, 2.4975, 2.9093]
 
-to_calc = 0.2
+to_calc = 1.0
+
+print(f"X = {X}")
+print()
+print(f"Y = {Y}")
+print()
+print(f"x = {to_calc}")
+print()
 
 print("First derivative in both first and second level accuracy:")
 print("(from left, from right, second level accuracy)")
